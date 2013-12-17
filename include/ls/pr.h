18ab488336fd088abb2c431e302bada82aae4dd4
Changli Gao <xiaosuo@gmail.com>
2013-12-18 00:20:48 +0800

    Don't require libsystemd-daemon

--- include/ls/pr.h
+++ include/ls/pr.h
@@ -2,7 +2,6 @@
 #define __LS_PR_H
 
 #include <stdio.h>
-#include <systemd/sd-daemon.h>
 
 static inline __attribute__((format(printf, 1, 2)))
 int no_pr(const char *fmt, ...)
@@ -13,27 +12,36 @@ int no_pr(const char *fmt, ...)
 __attribute__((format(printf, 1, 2)))
 extern int (*pr)(const char *fmt, ...);
 
+#define LS_EMERG   "<0>"  /* system is unusable */
+#define LS_ALERT   "<1>"  /* action must be taken immediately */
+#define LS_CRIT    "<2>"  /* critical conditions */
+#define LS_ERR     "<3>"  /* error conditions */
+#define LS_WARNING "<4>"  /* warning conditions */
+#define LS_NOTICE  "<5>"  /* normal but significant condition */
+#define LS_INFO    "<6>"  /* informational */
+#define LS_DEBUG   "<7>"  /* debug-level messages */
+
 #define pr_emerg(fmt, args...) \
-	pr(SD_EMERG fmt, ##args)
+	pr(LS_EMERG fmt, ##args)
 #define pr_alert(fmt, args...) \
-	pr(SD_ALERT fmt, ##args)
+	pr(LS_ALERT fmt, ##args)
 #define pr_crit(fmt, args...) \
-	pr(SD_CRIT fmt, ##args)
+	pr(LS_CRIT fmt, ##args)
 #define pr_err(fmt, args...) \
-	pr(SD_ERR fmt, ##args)
+	pr(LS_ERR fmt, ##args)
 #define pr_warning(fmt, args...) \
-	pr(SD_WARNING fmt, ##args)
+	pr(LS_WARNING fmt, ##args)
 #define pr_warn pr_warning
 #define pr_notice(fmt, args...) \
-	pr(SD_NOTICE fmt, ##args)
+	pr(LS_NOTICE fmt, ##args)
 #define pr_info(fmt, args...) \
-	pr(SD_INFO fmt, ##args)
+	pr(LS_INFO fmt, ##args)
 #ifdef NDEBUG
 #define pr_debug(fmt, args...) \
-	no_pr(SD_DEBUG fmt, ##args)
+	no_pr(LS_DEBUG fmt, ##args)
 #else
 #define pr_debug(fmt, args...) \
-	pr(SD_DEBUG fmt, ##args)
+	pr(LS_DEBUG fmt, ##args)
 #endif
 
 #endif /* __LS_PR_H */
