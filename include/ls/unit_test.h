6547d5d2c3fbc1a6fa63ddb69d5e7f5a5560195b
Changli Gao <xiaosuo@gmail.com>
2013-12-23 22:58:23 +0800

    UNIT_TEST() should accept 0 as the highest priority.

--- include/ls/unit_test.h
+++ include/ls/unit_test.h
@@ -4,7 +4,7 @@
 #ifdef NDEBUG
 #define UNIT_TEST(priority) static __attribute__((unused))
 #else
-#define UNIT_TEST(priority) static __attribute__((constructor(priority + 100)))
+#define UNIT_TEST(priority) static __attribute__((constructor(priority + 101)))
 #endif
 
 #endif /* __LS_UNIT_TEST_H */
