7565a69205dd272f17b7a9d80d0e8b1474d6ba56
Changli Gao <xiaosuo@gmail.com>
2014-01-15 20:39:34 +0800

    function attribute constructor(priority) is only valid after gcc-4.3

--- include/ls/unit_test.h
+++ include/ls/unit_test.h
@@ -2,9 +2,15 @@
 #define __LS_UNIT_TEST_H
 
 #ifdef NDEBUG
-#define UNIT_TEST(priority) static __attribute__((unused))
+#define UNIT_TEST static __attribute__((unused))
+#define UNIT_TEST1(priority) static __attribute__((unused))
 #else
-#define UNIT_TEST(priority) static __attribute__((constructor(priority + 101)))
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR >= 3)
+#define UNIT_TEST static __attribute__((constructor))
+#define UNIT_TEST1(priority) static __attribute__((constructor(priority + 101)))
+#else
+#define UNIT_TEST static __attribute__((constructor))
+#endif
 #endif
 
 #endif /* __LS_UNIT_TEST_H */
